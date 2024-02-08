5c4c7edf55f50cdfe1086d4df01f470b9d8abb0c
Pushpitha <pushpithagowda776@gmail.com>
2024-02-08 17:18:11 +0530

    Sass basics

---
+++ assignment2/script.js
@@ -0,0 +1,16 @@
+function tabs(evt, section) {
+    var i, secContent, secLinks;
+
+    secContent = document.getElementsByClassName("container2");
+    for (i=0; i<secContent.length; i++) {
+        secContent[i].style.display = "none";
+    }
+
+    secLinks = document.getElementsByClassName("row5block");
+    for(i=0; i<secLinks.length; i++) {
+        secLinks[i].className = secLinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(section).style.display = "block";
+    evt.currentTarget.className += " active";
+}
